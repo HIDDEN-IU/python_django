ccf65dfbf418dc271eef7267ae8cdd1d246e1125
HIDDEN-IU <87434875+HIDDEN-IU@users.noreply.github.com>
2022-10-16 13:44:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python_django
+ 백엔드를 위한 Django Rest Framework 교재 기반 프로젝트
---
+++ myvenv/Scripts/photo/views.py
@@ -0,0 +1,8 @@
+from unicodedata import name
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def photo_list(request):
+    return render(request, 'photo/photo_list.html', {})
